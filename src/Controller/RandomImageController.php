61c2621fcca69736af106679a619eceff874d1ef
Ludwig.Elatre <ludwig.elatre@ebp.com>
2024-05-29 09:28:13 +0200

    modification de l'affichage

--- src/Controller/RandomImageController.php
+++ src/Controller/RandomImageController.php
@@ -9,13 +9,13 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class RandomImageController extends AbstractController
 {
-    #[Route('/random-image', name: 'random_image')]
+    #[Route('/', name: 'random_image')]
     public function index(): Response
     {
         $randomImages = [];
         for ($i=0; $i<12; $i++) {
             $random = new Randomizer();
-            $randomImages[] .= $random->getInt(0, 100);
+            $randomImages[] .= "https://picsum.photos/300/200?random=".$random->getInt(0, 100);
         }
 
         return $this->render('random_image/index.html.twig', [
